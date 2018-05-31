ba7d9e5b4981c96c62a029c54b49df0f5a3bdbcf
mihirsam <xenon.of.x@gmail.com>
2018-05-31 18:06:00 +0530

    minor changes || not working

--- LabPrep/calcMain.java
+++ LabPrep/calcMain.java
@@ -11,22 +11,59 @@ class calcMain
     input = sc.next();
 
     int numArray[] = new int[input.length()];
-    int symArray[] = new int[input.length()];
+    char symArray[] = new char[input.length()];
     int flag1, flag2, numFlag = 0, symFlag = 0;
 
     for(int i=0; i<=input.length(); i++)
     {
-      if(a[i] == "+" || a[i] == "-" || a[i] == "*" || a[i] == "/")
+      if(input[i] == "+" || input[i] == "-" || input[i] == "*" || input[i] == "/")
       {
-        symArray[symFlag] = a[i];
+        symArray[symFlag] = input [i];
         symFlag++;
       }
       else
       {
-        numArray[numFlag] = parse.Int(a[i])
+        numArray[numFlag] = Interger.parseInt(input[i]);
         numFlag++;
       }
     }
-    System.out.println("Input : "+input+"\nLength : "+input.length());
+
+    int tempArray[] = new int[symFlag];
+    int num2 = 1, tempNum1 = Interger.parseInt(numArray[0]), tempNum2 = Interger.parseInt(numArray[num2]);
+
+    for(int i=0; i<symFlag; i++)
+    {
+      if(symArray[i] == '+')
+      {
+        tempNum1 = tempNum1 + tempNum2;
+        tempNum2 = numArray[num2++];
+      }
+
+      else if(symArray[i] == '-')
+      {
+        tempNum1 = tempNum1 - tempNum2;
+        tempNum2 = numArray[num2++];
+      }
+
+      else if(symArray[i] == '*')
+      {
+        tempNum1 = tempNum1 * tempNum2;
+        tempNum2 = numArray[num2++];
+      }
+
+      else if(symArray[i] == '/')
+      {
+        tempNum1 = tempNum1 / tempNum2;
+        tempNum2 = numArray[num2++];
+      }
+
+      else
+      {
+        System.out.println("Something Wrong!\nBye");
+        break;
+      }
+    }
+
+    System.out.println("Answer = "+tempNum1);
   }
 }
